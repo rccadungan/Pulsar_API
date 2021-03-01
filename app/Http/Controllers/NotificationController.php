265366b17a7eb3df4dbe206b302769e68fcd4187
Rhyss Brian Cadungan <rccadungan@student.apc.edu.ph>
2021-03-01 21:05:37 +0800

    Updated the NotificationController

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -9,41 +9,70 @@ class NotificationController extends Controller
 {
         //This public function will get all the 'notifications' table data. 
         public function getNotification() {
-            return response()->json(Notification::all(), 200);
+            return response()->json([
+                'success' => true,
+                'statusCode' => 200,
+                'message' => 'Successfully retrieved students notifications',
+                'data' => Notification::all()]);
         }
     
         //This public function will get specific 'notifications' table data using the unique ID. 
         public function getNotificationByID($id) {
             $notification = Notification::find($id);
             if (is_null($notification)) {
-                return response()->json(['message' => 'Notification not found'], 404);
+                return response()->json([
+                    'success' => false,
+                    'statusCode' => 404,
+                    'message' => 'Notification not found']);
             }
-            return response()->json($notification::find($id), 200);
+            return response()->json([
+                'success' => true,
+                'statusCode' => 200,
+                'message' => 'Successfully retrieved student notification',
+                'data' => $notification::find($id)]);
         }
         
         //This public function will add new notification to the 'notifications' table.
         public function addNotification(Request $request) {
             $notification = Notification::create($request->all());
-            return response()->json($notification, 201);
+            return response()->json([
+                'success' => true,
+                'statusCode' => 201,
+                'message' => 'Successfully added student notification',
+                'data' => $notification]);
         }
     
         //This public function will update a specific notification to the 'notifications' table using the unique ID.
         public function updateNotification(Request $request, $id) {
             $notification = Notification::find($id);
             if (is_null($notification)) {
-                return response()->json(['message' => 'Notification not found'], 404);
+                return response()->json([
+                    'success' => false,
+                    'statusCode' => 404,
+                    'message' => 'Notification not found']);
             }
             $notification->update($request->all());
-            return response($notification, 200);
+            return response([
+                'success' => true,
+                'statusCode' => 200,
+                'message' => 'Successfully updated student notification',
+                'data' =>$notification]);
         }
     
         //This function will delete a specific notification in the 'notifications' table. 
         public function deleteNotification(Request $request, $id) {
             $notification = Notification::find($id);
             if (is_null($notification)) {
-                return response()->json(['message' => 'Notification not found'], 404);
+                return response()->json([
+                    'success' => false,
+                    'statusCode' => 404,
+                    'message' => 'Notification not found']);
             }
             $notification ->delete();
-            return response()->json(null, 204);
+            return response()->json([
+                'success' => true,
+                'statusCode' => 204,
+                'message' => 'Successfully deleted student notification',
+                'data' => null]);
         }
 }
