7cbdec8d6c05430e4860392c0ba215ca71f2812b
Rhyss Brian Cadungan <rccadungan@student.apc.edu.ph>
2021-03-01 21:37:03 +0800

    Updated the StudentProfileController

--- app/Http/Controllers/StudentProfileController.php
+++ app/Http/Controllers/StudentProfileController.php
@@ -9,41 +9,70 @@ class StudentProfileController extends Controller
 {
         //This public function will get all the 'student_profiles' table data. 
         public function getStudentProfile() {
-            return response()->json(StudentProfile::all(), 200);
+            return response()->json([
+                'success' => true,
+                'statusCode' => 200,
+                'message' => 'Successfully retrieved students profiles',
+                'data' => StudentProfile::all()]);
         }
     
         //This public function will get specific 'students_profiles' table data using the unique ID. 
         public function getStudentProfileById($id) {
             $student_profile = StudentProfile::find($id);
             if (is_null($student_profile)) {
-                return response()->json(['message' => 'Student not found'], 404);
+                return response()->json([
+                    'success' => false,
+                    'statusCode' => 404,
+                    'message' => 'Student Profile not found']);
             }
-            return response()->json($student_profile::find($id), 200);
+            return response()->json([
+                'success' => true,
+                'statusCode' => 200,
+                'message' => 'Successfully retrieved student profile',
+                'data' => $student_profile::find($id)]);
         }
         
         //This public function will add new student data to the 'students_profiles' table.
         public function addStudentProfile(Request $request) {
             $student_profile = StudentProfile::create($request->all());
-            return response()->json($student_profile, 201);
+            return response()->json([
+                'success' => true,
+                'statusCode' => 201,
+                'message' => 'Successfully added student profile',
+                'data' => $student_profile]);
         }
     
         //This public function will update a specific student data to the 'students_profiles' table using the unique ID.
         public function updateStudentProfile(Request $request, $id) {
             $student_profile = StudentProfile::find($id);
             if (is_null($student_profile)) {
-                return response()->json(['message' => 'Student not found'], 404);
+                return response()->json([
+                    'success' => false,
+                    'statusCode' => 404,
+                    'message' => 'Student Profile not found']);
             }
             $student_profile->update($request->all());
-            return response($student_profile, 200);
+            return response([
+                'success' => true,
+                'statusCode' => 200,
+                'message' => 'Successfully updated student profile',
+                'data' => $student_profile]);
         }
     
         //This function will delete a specific student data in the 'students_profiles' table. 
         public function deleteStudentProfile(Request $request, $id) {
             $student_profile = StudentProfile::find($id);
             if (is_null($student_profile)) {
-                return response()->json(['message' => 'Student not found'], 404);
+                return response()->json([
+                    'success' => false,
+                    'statusCode' => 404,
+                    'message' => 'Student Profile not found']);
             }
             $student_profile->delete();
-            return response()->json(null, 204);
+            return response()->json([
+                'success' => true,
+                'statusCode' => 204,
+                'message' => 'Successfully deleted student profile',
+                'data' => null]);
         }
 }
