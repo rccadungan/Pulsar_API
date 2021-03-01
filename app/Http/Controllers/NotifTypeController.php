a9f60fd1e659eea6343cd3835f5b7149057e94af
Rhyss Brian Cadungan <rccadungan@student.apc.edu.ph>
2021-03-01 21:09:08 +0800

    Updated the NotificationTypeController

--- app/Http/Controllers/NotifTypeController.php
+++ app/Http/Controllers/NotifTypeController.php
@@ -9,41 +9,70 @@ class NotifTypeController extends Controller
 {
         //This public function will get all the 'notif_types' table data. 
         public function getNotifType() {
-            return response()->json(NotifType::all(), 200);
+            return response()->json([
+                'success' => true,
+                'statusCode' => 200,
+                'message' => 'Successfully retrieved notifications types',
+                'data' =>NotifType::all()]);
         }
     
         //This public function will get specific 'notif_types' table data using the unique ID. 
         public function getNotifTypeById($id) {
             $notif_type = NotifType::find($id);
             if (is_null($notif_type)) {
-                return response()->json(['message' => 'Notification Type not found'], 404);
+                return response()->json([
+                    'success' => false,
+                    'statusCode' => 404,
+                    'message' => 'Notification Type not found']);
             }
-            return response()->json($notif_type::find($id), 200);
+            return response()->json([
+                'success' => true,
+                'statusCode' => 200,
+                'message' => 'Successfully retrieved notification type',
+                'data' => $notif_type::find($id)]);
         }
         
         //This public function will add new notification type to the 'notif_types' table.
         public function addNotifType(Request $request) {
             $notif_type = NotifType::create($request->all());
-            return response()->json($notif_type, 201);
+            return response()->json([
+                'success' => true,
+                'statusCode' => 201,
+                'message' => 'Successfully added notification type',
+                'data' => $notif_type]);
         }
     
         //This public function will update a specific notification type to the 'notif_types' table using the unique ID.
         public function updateNotifType(Request $request, $id) {
             $notif_type = NotifType::find($id);
             if (is_null($notif_type)) {
-                return response()->json(['message' => 'Notification Type not found'], 404);
+                return response()->json([
+                    'success' => false,
+                    'statusCode' => 404,
+                    'message' => 'Notification Type not found']);
             }
             $notif_type->update($request->all());
-            return response($notif_type, 200);
+            return response([
+                'success' => true,
+                'statusCode' => 200,
+                'message' => 'Successfully updated notification type',
+                'data' => $notif_type]);
         }
     
         //This function will delete a specific notification type in the 'notif_types' table. 
         public function deleteNotifType(Request $request, $id) {
             $notif_type= NotifType::find($id);
             if (is_null($notif_type)) {
-                return response()->json(['message' => 'Notification Type not found'], 404);
+                return response()->json([
+                    'success' => false,
+                    'statusCode' => 404,
+                    'message' => 'Notification Type not found']);
             }
             $notif_type->delete();
-            return response()->json(null, 204);
+            return response()->json([
+                'success' => true,
+                'statusCode' => 204,
+                'message' => 'Successfully deleted notification type',
+                'data' => null]);
         }
 }
