25984a00b6d463a359c5182b361101cad02371c5
Rhyss Brian Cadungan <rccadungan@student.apc.edu.ph>
2021-03-01 21:01:48 +0800

    Updated the GradeController

--- app/Http/Controllers/GradeController.php
+++ app/Http/Controllers/GradeController.php
@@ -9,41 +9,70 @@ class GradeController extends Controller
 {
         //This public function will get all the 'grades' table data. 
         public function getGrade() {
-            return response()->json(Grades::all(), 200);
+            return response()->json([
+                'success' => true,
+                'statusCode' => 200,
+                'message' => 'Successfully retrieved students grades',
+                'data' => Grades::all()]);
         }
     
         //This public function will get specific 'grades' table data using the unique ID. 
         public function getGradeByID($id) {
             $grade = Grades::find($id);
             if (is_null($grade)) {
-                return response()->json(['message' => 'Grade not found'], 404);
+                return response()->json([
+                    'success' => false,
+                    'statusCode' => 404,
+                    'message' => 'Grade not found']);
             }
-            return response()->json($grade::find($id), 200);
+            return response()->json([
+                'success' => true,
+                'statusCode' => 200,
+                'message' => 'Successfully retrieved student grade',
+                'data' => $grade::find($id)]);
         }
         
         //This public function will add new grade to the 'grades' table.
         public function addGrade(Request $request) {
             $grade = Grades::create($request->all());
-            return response()->json($grade, 201);
+            return response()->json([
+                'success' => true,
+                'statusCode' => 201,
+                'message' => 'Successfully updated student grade',
+                'data' => $grade]);
         }
     
         //This public function will update a specific grade to the 'grades' table using the unique ID.
         public function updateGrade(Request $request, $id) {
             $grade = Grades::find($id);
             if (is_null($grade)) {
-                return response()->json(['message' => 'Grade not found'], 404);
+                return response()->json([
+                    'success' => false,
+                    'statusCode' => 404,
+                    'message' => 'Grade not found']);
             }
             $grade->update($request->all());
-            return response($grade, 200);
+            return response([
+                'success' => true,
+                'statusCode' => 200,
+                'message' => 'Successfully updated student grade',
+                'data' => $grade]);
         }
     
         //This function will delete a specific grade in the 'grades' table. 
         public function deleteGrade(Request $request, $id) {
             $grade = Grades::find($id);
             if (is_null($grade)) {
-                return response()->json(['message' => 'Grade not found'], 404);
+                return response()->json([
+                    'success' => false,
+                    'statusCode' => 404,
+                    'message' => 'Grade not found']);
             }
             $grade ->delete();
-            return response()->json(null, 204);
+            return response()->json([
+                'success' => true,
+                'statusCode' => 204,
+                'message' => 'Successfully deleted student grade',
+                'data' => null]);
         }
 }
