dd4ead2b0905cc1c728a3f4e92a38e313c0a4172
Rhyss Brian Cadungan <rccadungan@student.apc.edu.ph>
2021-03-01 21:39:57 +0800

    Updated the TermController

--- app/Http/Controllers/TermController.php
+++ app/Http/Controllers/TermController.php
@@ -9,41 +9,70 @@ class TermController extends Controller
 {
         //This public function will get all the 'terms' table data. 
         public function getTerm() {
-            return response()->json(Term::all(), 200);
+            return response()->json([
+                'success' => true,
+                'statusCode' => 200,
+                'message' => 'Successfully retrieved terms',
+                'data' => Term::all()]);
         }
     
         //This public function will get specific 'terms' table data using the unique ID. 
         public function getTermByID($id) {
             $term = Term::find($id);
             if (is_null($term)) {
-                return response()->json(['message' => 'Term not found'], 404);
+                return response()->json([
+                    'success' => false,
+                    'statusCode' => 404,
+                    'message' => 'Term not found']);
             }
-            return response()->json($term::find($id), 200);
+            return response()->json([
+                'success' => true,
+                'statusCode' => 200,
+                'message' => 'Successfully retrieved term',
+                'data' => $term::find($id)]);
         }
         
         //This public function will add new term to the 'terms' table.
         public function addTerm(Request $request) {
             $term = Term::create($request->all());
-            return response()->json($term, 201);
+            return response()->json([
+                'success' => true,
+                'statusCode' => 201,
+                'message' => 'Successfully added term',
+                'data' => $term]);
         }
     
         //This public function will update a specific term to the 'terms' table using the unique ID.
         public function updateTerm(Request $request, $id) {
             $term = Term::find($id);
             if (is_null($term)) {
-                return response()->json(['message' => 'Term not found'], 404);
+                return response()->json([
+                    'success' => false,
+                    'statusCode' => 404,
+                    'message' => 'Term not found']);
             }
             $term->update($request->all());
-            return response($term, 200);
+            return response([
+                'success' => true,
+                'statusCode' => 200,
+                'message' => 'Successfully updated term',
+                'data' => $term]);
         }
     
         //This function will delete a specific term in the 'terms' table. 
         public function deleteTerm(Request $request, $id) {
             $term = Term::find($id);
             if (is_null($term)) {
-                return response()->json(['message' => 'Term not found'], 404);
+                return response()->json([
+                    'success' => false,
+                    'statusCode' => 404,
+                    'message' => 'Term not found']);
             }
             $term ->delete();
-            return response()->json(null, 204);
+            return response()->json([
+                'success' => true,
+                'statusCode' => 204,
+                'message' => 'Successfully deleted term',
+                'data' => null]);
         }
 }
