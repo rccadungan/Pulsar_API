594aac817170c39bab2b39ccdbd2dd2051e8d41b
Rhyss Brian Cadungan <rccadungan@student.apc.edu.ph>
2021-03-01 20:56:56 +0800

    Updated the DepartmentController

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -9,41 +9,70 @@ class DepartmentController extends Controller
 {
         //This public function will get all the 'departments' table data. 
         public function getDepartment() {
-            return response()->json(Department::all(), 200);
+            return response()->json([
+                'success' => true,
+                'statusCode' => 200,
+                'message' => 'Successfully retrieved clearances departments',
+                'data' => Department::all()]);
         }
     
         //This public function will get specific 'departments' table data using the unique ID. 
         public function getDepartmentByID($id) {
             $department = Department::find($id);
             if (is_null($department)) {
-                return response()->json(['message' => 'Department not found'], 404);
+                return response()->json([
+                    'success' => false,
+                    'statusCode' => 404,
+                    'message' => 'Department not found']);
             }
-            return response()->json($department::find($id), 200);
+            return response()->json([
+                'success' => true,
+                'statusCode' => 200,
+                'message' => 'Successfully retrieved clearance department',
+                'data' => $department::find($id)]);
         }
         
         //This public function will add new department to the 'departments' table.
         public function addDepartment(Request $request) {
             $department = Department::create($request->all());
-            return response()->json($department, 201);
+            return response()->json([
+                'success' => true,
+                'statusCode' => 201,
+                'message' => 'Successfully added clearance department',
+                'data' => $department]);
         }
     
         //This public function will update a specific department to the 'departments' table using the unique ID.
         public function updateDepartment(Request $request, $id) {
             $department = Department::find($id);
             if (is_null($department)) {
-                return response()->json(['message' => 'Department not found'], 404);
+                return response()->json([
+                    'success' => false,
+                    'statusCode' => 404,
+                    'message' => 'Department not found']);
             }
             $department->update($request->all());
-            return response($department, 200);
+            return response([
+                'success' => true,
+                'statusCode' => 200,
+                'message' => 'Successfully updated clearance department',
+                'data' => $department]);
         }
     
         //This function will delete a specific department in the 'departments' table. 
         public function deleteDepartment(Request $request, $id) {
             $department = Department::find($id);
             if (is_null($department)) {
-                return response()->json(['message' => 'Department not found'], 404);
+                return response()->json([
+                    'success' => false,
+                    'statusCode' => 404,
+                    'message' => 'Department not found']);
             }
             $department ->delete();
-            return response()->json(null, 204);
+            return response()->json([
+                'success' => true,
+                'statusCode' => 204,
+                'message' => 'Successfully deleted clearance department',
+                'data' => null]);
         }
 }
