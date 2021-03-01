ca908cfe92fdee9d027a73a993dde1dd689c40c8
Rhyss Brian Cadungan <rccadungan@student.apc.edu.ph>
2021-03-01 21:26:02 +0800

    Updated the PreRegisteredSubjectDetailController

--- app/Http/Controllers/PreRegisteredSubjectDetailController.php
+++ app/Http/Controllers/PreRegisteredSubjectDetailController.php
@@ -9,41 +9,70 @@ class PreRegisteredSubjectDetailController extends Controller
 {
         //This public function will get all the 'pre_registered_subject_details' table data. 
         public function getPreRegisteredSubjectDetail() {
-            return response()->json(PreRegisteredSubjectDetail::all(), 200);
+            return response()->json([
+                'success' => true,
+                'statusCode' => 200,
+                'message' => 'Successfully retrieved students pre-registered subject details',
+                'data' => PreRegisteredSubjectDetail::all()]);
         }
     
         //This public function will get specific 'pre_registered_subject_details' table data using the unique ID. 
         public function getPreRegisteredSubjectDetailByID($id) {
             $pre_registered_subject_detail = PreRegisteredSubjectDetail::find($id);
             if (is_null($pre_registered_subject_detail)) {
-                return response()->json(['message' => 'Pre-registered Subject Detail not found'], 404);
+                return response()->json([
+                    'success' => false,
+                    'statusCode' => 404,
+                    'message' => 'Pre-registered Subject Details not found']);
             }
-            return response()->json($pre_registered_subject_detail::find($id), 200);
+            return response()->json([
+                'success' => true,
+                'statusCode' => 200,
+                'message' => 'Successfully retrieved student pre-registered subject detail',
+                'data' => $pre_registered_subject_detail::find($id)]);
         }
         
         //This public function will add new pre registered subject detail to the 'pre_registered_subject_details' table.
         public function addPreRegisteredSubjectDetail(Request $request) {
             $pre_registered_subject_detail = PreRegisteredSubjectDetail::create($request->all());
-            return response()->json($pre_registered_subject_detail, 201);
+            return response()->json([
+                'success' => true,
+                'statusCode' => 201,
+                'message' => 'Successfully added student pre-registered subject detail',
+                'data' => $pre_registered_subject_detail]);
         }
     
         //This public function will update a specific pre registered subject detail to the 'pre_registered_subject_details' table using the unique ID.
         public function updatePreRegisteredSubjectDetail(Request $request, $id) {
             $pre_registered_subject_detail = PreRegisteredSubjectDetail::find($id);
             if (is_null($pre_registered_subject_detail)) {
-                return response()->json(['message' => 'Pre-registered Subject Detail not found'], 404);
+                return response()->json([
+                    'success' => false,
+                    'statusCode' => 404,
+                    'message' => 'Pre-registered Subject Details not found']);
             }
             $pre_registered_subject_detail->update($request->all());
-            return response($pre_registered_subject_detail, 200);
+            return response([
+                'success' => true,
+                'statusCode' => 200,
+                'message' => 'Successfully updated student pre-registered subject detail',
+                'data' => $pre_registered_subject_detail]);
         }
     
         //This function will delete a specific pre registered subject detail in the 'pre_registered_subject_details' table. 
         public function deletePreRegisteredSubjectDetail(Request $request, $id) {
             $pre_registered_subject_detail = PreRegisteredSubjectDetail::find($id);
             if (is_null($pre_registered_subject_detail)) {
-                return response()->json(['message' => 'Pre-registered Subject Detail not found'], 404);
+                return response()->json([
+                    'success' => false,
+                    'statusCode' => 404,
+                    'message' => 'Pre-registered Subject Details not found']);
             }
             $pre_registered_subject_detail ->delete();
-            return response()->json(null, 204);
+            return response()->json([
+                'success' => true,
+                'statusCode' => 204,
+                'message' => 'Successfully deleted student pre-registered subject detail',
+                'data' => null]);
         }
 }
